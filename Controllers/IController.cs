4b070ea242e3c748d5013b7ac22e828339354212
Hans Tremmel <hans.tremmel@gmail.com>
2016-03-27 17:20:32 -0700

    Abandoned, doesnt seem to play with access.

--- Controllers/IController.cs
+++ Controllers/IController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InvoiceTracker.Views;
+using InvoiceTracker.Database;
 
 
 namespace InvoiceTracker.Controllers
 {
+	public interface IController
+	{
+		IInvoiceTrackerDb Model { get; }
+		IView View { get; }
+		
+		void OnClick();
+		void OnExit();
+		void OnEnter();		
+	}
 
 }
